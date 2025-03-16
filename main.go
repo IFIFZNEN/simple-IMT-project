c60a43c7b90e3c013d0e9c9b7dd39dce804fbd19
IFIFZNEN <temur.artikov@kapitalbank.uz>
2025-03-16 19:58:24 +0500

    2nd version

--- main.go
+++ main.go
@@ -9,18 +9,20 @@ func main() {
 	const IMTPower = 2
 	var userHeight float64 // рост
 	var userKg float64     // вес
-	fmt.Print("⚖️___ Калькулятор индекса массы тела ___ ⚖️\n")
-	fmt.Print("Enter your user height (в метрах): ")
+	fmt.Println("⚖️___ Калькулятор индекса массы тела ___ ⚖️")
+	fmt.Print("Enter your user height (в сантиметрах): ")
 	_, err := fmt.Scan(&userHeight)
 	if err != nil {
 		fmt.Printf("Вы неправильно ввели ваш рост. Например: 1.92 ERROR: %s", err)
 		return
 	}
-	fmt.Println("Enter your user kg: ")
+	fmt.Print("Enter your user kg: ")
 	_, err = fmt.Scan(&userKg)
 	if err != nil {
 		fmt.Printf("Вы неправильно ввели ваш вес. Например: 90 ERROR: %s", err)
 	}
-	IMT := userKg / math.Pow(userHeight, IMTPower)
-	fmt.Printf("Ваш индекс массы тела: %f", IMT)
+	IMT := userKg / math.Pow(userHeight/100, IMTPower)
+	result := fmt.Sprintf("Ваш индекс массы тела: %0.f", IMT)
+	fmt.Print(result)
+
 }
